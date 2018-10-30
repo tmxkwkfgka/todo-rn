95185409cecf259a326ed0d46f34920744d32ffb
jiji <jjh508@gmail.com>
2018-10-30 19:56:38 +0900

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, TextInput,
+Platform } from 'react-native';
+const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>todo-rn</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"now todo"}></TextInput>
+        </View>
       </View>
     );
   }
@@ -14,8 +20,38 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+  title:{
+    color:"white",
+    fontSize:30,
+    marginTop: 40,
+    fontWeight:"400",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex:1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    
+    ...Platform.select({
+      ios:{
+        shadowColor:"rgba(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset:{
+          height:-1,
+          width:0
+        }
+      },
+      android:{
+        elevation:3
+      }
+
+    })
+  }
 });
